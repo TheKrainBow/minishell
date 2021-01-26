218655bb73ff743f922ebb331fe6eb4eb0d35de5
Sagow <marina.delwaulle@free.fr>
2021-01-26 21:42:24 +0100

    builtin cd, fonctionnel mais avec encore ses printf

--- marina/srcs/builtins/cd.c
+++ marina/srcs/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: magostin <magostin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marina <marina@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 16:23:43 by marina            #+#    #+#             */
-/*   Updated: 2021/01/26 18:08:39 by magostin         ###   ########.fr       */
+/*   Updated: 2021/01/26 21:37:03 by marina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /*#include "../../includes/minishell.h"*/
 
-char		**ft_split(char const *s, char c);
 int	pwd(void)
 {
 	char	*path;
@@ -30,51 +30,91 @@ int	pwd(void)
 	return (0);
 }
 
-int	cd(/*t_data *data,*/char *arg)
-{
-	pwd();
-	printf("%d\n",chdir(arg));
-	pwd();
-}
 
-char	*quote_bin(char *path)
+
+char	*dequote(char *arg, int first, int second)
 {
-	int	i;
-	int	sq;
-	int	wq;
 	char	*new;
+	int		i;
 
+	if (!(new = malloc(sizeof(char) * (strlen(arg) - 1))))
+		return (NULL);
 	i = 0;
-	sq = 0;
-	wq = 0;
-	while (path[i] && sq < 2 && wq < 2)
+	while (i < first)
 	{
-		if (path[i] == '\'')
-			sq++;
-		if (path[i] == '"')
-			wq++;
+		new[i] = arg[i];
 		i++;
 	}
-	if (sq < 2 && wq < 2)
-		return (path);
-	if (!(new = malloc(ft_strlen())))
+	while (i < second - 1)
+	{
+		new[i] = arg[i + 1];
+		i++;
+	}
+	while (i < strlen(arg) - 1)
+	{
+		new[i] = arg[i + 2];
+		i++;
+	}
+	new[i] = '\0';
+	free (arg);
+	return (new);
+}
+
+char	*quote_bin(char *arg)
+{
+	int		i;
+	int		sq;
+	int		wq;
+
+	i = 0;
+	sq = -1;
+	wq = -1;
+	while (arg[i] && ((arg[i] != '"' && arg[i] != '\'')
+	|| (arg[i] == '"' && wq == -1) || (arg[i] == '\'' && sq == -1)))
+	{
+		if (arg[i] == '"')
+			wq = i;
+		if (arg[i] == '\'')
+			sq = i;
+		i++;
+	}
+	if ((arg[i] == '"' && wq != -1) || (arg[i] == '\'' && sq != -1))
+	{
+		if (arg[i] == '"')
+			arg = dequote(arg, wq, i);
+		else
+			arg = dequote(arg, sq, i);
+		arg = quote_bin(arg);
+	}
+	return (arg);
+}
+
+int	cd(/*t_data *data,*/char *arg)
+{
+	char	*path;
+
+	pwd();
+	path = quote_bin(arg);
+	printf("res quote_bin = %s\n", path);
+	printf("%d\n",chdir(path));
+	pwd();
 }
 
 int	main(void)
 {
-	char	*arg = "test/../..";
-	char	**path;
+	char	*arg = ".'./'l'exer'";
+	char	*path;
 	int		i;
 
-	path = ft_split(arg, '/');
+	if (!(path = malloc(sizeof(char) * (strlen(arg) + 1))))
+		return (-1);
 	i = 0;
-	while (path[i])
+	while (arg[i])
 	{
-		path[i] = quote_bin(path[i]);
-		cd(path[i]);
-		free(path[i]);
+		path[i] = arg[i];
 		i++;
 	}
-	free(path);
+	path[i] = '\0';
+	i = cd(path);
 	return (0);
 }
