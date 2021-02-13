af4d39a253014e0b623ca4b49abb0d6fd2dd546b
Sagow <marina.delwaulle@free.fr>
2021-02-13 23:47:26 +0100

    echo presque bon, manque -nnnnn

--- marina/srcs/builtins/echo.c
+++ marina/srcs/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: magostin <magostin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mdelwaul <mdelwaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:10:15 by mdelwaul          #+#    #+#             */
-/*   Updated: 2021/02/13 22:51:00 by magostin         ###   ########.fr       */
+/*   Updated: 2021/02/13 23:38:46 by mdelwaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@
 
 int		ft_find_in_env(char *name);
 
-void	print_esc(char *word, int *i)
+int		print_esc(char *word, int *i)
 {
 	if (word[*i + 1])
 	{
 		printf("%c", word[*i + 1]);
 		*i += 2;
+		return (1);
 	}
+	return (0);
 }
 
 char	*get_name(char *word, int *i)
@@ -41,7 +43,7 @@ char	*get_name(char *word, int *i)
 	while (word && word[*i] && ft_isalnum(word[*i]))
 	{
 		size++;
-		*i = *i + 1;
+		(*i)++;
 	}
 	if (!(name = malloc(sizeof(char) * size)))
 		return (NULL);
@@ -49,55 +51,67 @@ char	*get_name(char *word, int *i)
 	return (name);
 }
 
-void	print_var(char *word, int *i)
+int		print_var(char *word, int *i)
 {
 	char	*name;
 	int		number;
 	char	*content;
 
 	if (!(name = get_name(word, i)))
-		return ;
+		return (0) ;
 	number = ft_find_in_env(name);
 	free(name);
 	if (number < 0)
-		return ;
+		return (0);
 	content = ft_strchr(__environ[number], '=') + 1;
 	printf("%s", content++);
+	return(ft_strlen(content));
 }
 
-void	printing(char *word)
+int		printing(char *word)
 {
 	int		strong;
 	int		i;
+	int		printed;
 
 	strong = 0;
+	printed = 0;
 	i = 0;
 	while (word[i])
 	{
 		if (word[i] == '\\')
-			print_esc(&word[i], &i);
+			printed += print_esc(&word[i], &i);
 		if (word[i] == '\'')
+		{
 			strong = (strong + 1) % 2;
+			i++;
+		}
 		if (!strong && word[i] == '$')
-			print_var(&word[i], &i);
+			printed += print_var(&word[i], &i);
 		if (!strong && word[i] == '"')
 			i++;
-		printf("%c", word[i]);
-		i++;
+		if (word[i])
+		{
+			printf("%c", word[i]);
+			printed++;
+			i++;
+		}
 	}
+	return (printed);
 }
 
 int		echo(t_cmd *cmd)
 {
 	int		i;
+	int		printed;
 
 	i = 1;
 	if (!ft_strncmp(cmd->args[1], "-n", 3))
 		i++;
 	while (cmd->args[i])
 	{
-		printing(cmd->args[i]);
-		if (cmd->args[++i])
+		printed = printing(cmd->args[i]);
+		if (cmd->args[++i] && printed)
 			printf(" ");
 	}
 	if (ft_strncmp(cmd->args[1], "-n", 3))
