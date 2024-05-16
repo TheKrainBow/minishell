23001268cd9c20bac1bf12280cc9b32bb7eaaedc
Giorgi Gochitashvili <ggochita@c1r6p2.42nice.fr>
2024-05-16 17:37:45 +0200

    Wip

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -12,8 +12,34 @@
 
 #include "minishell.h"
 
+int	check_env_name(char *env_name)
+{
+	int	i;
+
+	i = 1;
+	if (!is_alpha(env_name[0]) && env_name[0] != '_')
+		return (0);
+	while (env_name[i] != '=')
+	{
+		if (!is_alpha(env_name[i]) && !is_digit(env_name[i]))
+		{
+			if (env_name[i] != ' ')
+				return (0);
+		}
+	}
+}
+
 int 	ft_export(t_cmd *cmd)
 {
 	(void)cmd;
+	int	i;
+
+	i = 1;
+	while (cmd->args[i] != NULL)
+	{
+		if (!check_env_name(cmd->args[i]))
+			return (0);
+		
+	}
 	return (1);
 }
