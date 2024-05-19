5edf44f9b2ff6d6eb934c881f07492346e1f8da0
ggochiat <ggochita@gamil.com>
2024-05-19 05:44:05 +0200

    pwd is done

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -12,8 +12,27 @@
 
 #include "minishell.h"
 
+
+void	print_pwd(t_cmd *cmd, char *name)
+{
+	int	i;
+
+	i = 0;
+	while (cmd->data->env[i])
+	{
+		if (ft_strncmp(cmd->data->env[i], name,
+					ft_strchr(cmd->data->env[i], '=') - cmd->data->env[i]) == 0)
+		{
+			printf("%s\n", ft_strchr(cmd->data->env[i], '=') + 1);
+			return ;
+		}
+		i++;
+	}
+	return ;
+}
+
 int	ft_pwd(t_cmd *cmd)
 {
-	(void)cmd;
+	print_pwd(cmd, "PWD");
 	return (1);
 }
